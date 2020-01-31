e70e808d4191106ca70183a95330c18e11539885
zhangjiahui <jhzhang97@163.com>
2020-01-31 11:06:02 +0800

    digui

---
+++ data_stucture/digui/exhaustive_search.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int n, a[50];
+
+int solve (int i, int m) {
+    if (m == 0) 
+        return 1;
+    if (i >= n) 
+        return 0;
+    int res = solve(i++, m - a[i]);
+    return res;
+}
+
+int main()
+{
+    int q, M, i;
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", & a[i]);
+    scanf("%d", &q);
+    for (i = 0; i < q; i++) {
+        scanf("%d", &M);
+        if (solve(0, M))
+            printf("yes\n");
+        else
+            printf("no\n");
+    }
+    return 0;
+}
